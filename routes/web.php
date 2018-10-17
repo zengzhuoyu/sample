614ba7be5cd3de7abaceb7c8686715150eb3f773
zengzhuoyu <zengzhuoyu24@163.com>
2018-10-17 21:42:10 +0800

    第7章 结束

--- routes/web.php
+++ routes/web.php
@@ -17,3 +17,7 @@ Route::get('/about', 'StaticPagesController@about')->name('about');//关于页
 
 Route::get('/signup', 'UsersController@create')->name('signup');//注册页
 Route::resource('users', 'UsersController');
+
+Route::get('/login', 'SessionsController@create')->name('login');
+Route::post('/login', 'SessionsController@store')->name('login');
+Route::delete('/logout', 'SessionsController@destroy')->name('logout');
